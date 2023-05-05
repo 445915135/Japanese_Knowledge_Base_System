2432d251f93b5b861b19488b0ac9d81c8bb35913
miaohao <>
2023-05-05 14:31:20 +0800

    first commit

---
+++ web/src/views/about.vue
@@ -0,0 +1,27 @@
+<template>
+  <a-layout>
+    <a-layout-content :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }">
+      <div class="about">
+        <h2>我是苗浩，热爱编程</h2>
+        <div><b>我喜欢去研究一些新的技术</b></div>
+        <br>
+        </div>
+    </a-layout-content>
+  </a-layout>
+</template>
+
+<style>
+  .about {
+    line-height: 30px;
+    padding: 40px;
+    background-color: #f5f5f5;
+    font-size: 16px;
+  }
+  .about h2 {
+    margin-bottom: 30px;
+    font-weight: 700;
+  }
+  .about img {
+    margin-top: 10px;
+  }
+</style>
